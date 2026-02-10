ad583b2c4cddbfdb2c5eb2cde2a31b542df9e28b
Erick Mancillas <albertommjm10@gmail.com>
2026-02-09 21:05:49 -0600

    V 1.1

--- Design.py
+++ Design.py
@@ -9,17 +9,18 @@ class datos:
     def setNombre(self,nombre):
         self.nombre=nombre
     def getNombre(self):
-        return self.nombre  
-
+        return self.nombre 
+     
+#Ventana principal de la app
 class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
+    def __init__(self): 
+        super().__init__() #Ventana principal de la app
         self.nombre=""
         self.config(width=200,height=200)
         self.title("Practica 1")
         self.label_nombre=tk.Label(self,text="Ingrese Nombre:")
         self.label_nombre.place(x=60,y=0)
-        self.entry_nombre=tk.Entry(self,width=20)
+        self.entry_nombre=tk.Entry(self,width=20) #Campo de texto para ingresar el nombre
         self.entry_nombre.place(x=60,y=40)
         self.boton_mostrar=tk.Button(self,text="Mostrar",command=lambda:self.mostrar())
         self.boton_mostrar.place(x=60,y=80)
@@ -32,14 +33,5 @@ class App(tk.Tk):
         messagebox.showinfo(title="Datos",message=salida)
         
 if __name__=="__main__":
-    app=App()
-    app.mainloop()
-        
-        
-class datos: 
-    def __init__(self):
-        self.nombre=""
-    def setNombre(self,nombre):
-        self.nombre=nombre
-    def getNombre(self):
-        return self.nombre
+        app=App()
+        app.mainloop()
